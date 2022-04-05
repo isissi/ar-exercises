28852a301265f9ec21a2ffd23df0ead2dc926ce0
isissi <isissi_525@hotmail.com>
2022-04-05 11:06:24 -0600

    delete the third store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+
+@store2 = Store.second
+
+@store1.name = 'Coquitlam'
+@store1.annual_revenue = 200000
+
+puts @store1.name
+puts @store1.annual_revenue
