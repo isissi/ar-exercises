52e0bbbdd0025aadc66c96b619335047c759156b
isissi <isissi_525@hotmail.com>
2022-04-05 11:24:02 -0600

    add 3 stores & filter mens & womens

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: 'Surrey', annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+
+whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
+
+yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, womens_apparel: false, mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# Mens Stores
+@mens_stores.each do |store|
+  puts "name: #{store.name}  annual revenue: #{store.annual_revenue}"
+end
+
+
+@womens_stores = Store.where(["womens_apparel = ? and annual_revenue > ?", "true", "1000000"])
+
+# Womens Stores with less than 1M annual revenue
+@womens_stores.each do |store|
+  puts "name: #{store.name}  annual revenue: #{store.annual_revenue}"
+end
